296acbd3d703dc3684d9b968c3e32517c2c70379
Rubens Rissi Onzi <rubensrissi@hotmail.com>
2023-11-11 17:45:38 +0100

    Make merge more resilient

--- merge.py
+++ merge.py
@@ -1,20 +1,38 @@
 import pandas as pd
 from os import walk
-from collections import defaultdict
+import argparse
 
-listis = [] 
-result_df = pd.DataFrame()
-for (dirpath, dirnames, filenames) in walk("GSE235915_counts_lists"):
-    listis.extend(filenames)
-    break
-i = 0
-for files in listis:
-    df_trips =  pd.read_csv(f'GSE235915_counts_lists/{files}', delimiter="\t",header=None)
-    df_trips = df_trips.iloc[4:]
-    if(i==0):
-        f_column = df_trips.iloc[:, [0]]
+
+def main(folder: str, listis: list):
+    listis.sort()
+    result_df = pd.DataFrame()
+
+    i = 0
+    for file in listis:
+        df_trips =  pd.read_csv(f'{folder}/{file}', delimiter="\t",header=None)
+        df_trips = df_trips.iloc[4:]
+        if(i==0):
+            f_column = df_trips.iloc[:, [0]]
+            result_df = pd.concat([result_df,f_column], axis = 1)
+        f_column = df_trips.iloc[:, [1]]
         result_df = pd.concat([result_df,f_column], axis = 1)
-    f_column = df_trips.iloc[:, [1]]
-    result_df = pd.concat([result_df,f_column], axis = 1)
-    i+=1
-result_df.to_csv("GSE235915.txt", index=False, header=None)
+        i+=1
+
+    result_df.to_csv(f"{folder.split(sep="_")[0]}.txt", index=False, header=None)
+
+
+if __name__  == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder', metavar='path', required=True,
+    help='folder points')
+    
+    args = parser.parse_args()
+    
+    listis = []
+    
+    for (dirpath, dirnames, filenames) in walk(args.folder):
+        listis.extend(filenames)
+        break
+    
+    main(args.folder, listis)
